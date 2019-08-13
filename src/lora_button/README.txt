9cc6ef68681eeeb86c1b85934c8148946629fb2f
Mitchell Bright <mitch@newie.ventures>
2019-08-13 17:55:54 +1000

    Added firmware for LoRa button (mDot based).

---
+++ src/lora_button/README.txt
@@ -0,0 +1,13 @@
+Use the mbed online IDE to compile the code with the following libraries:
+
+libmDot
+========
+https://os.mbed.com/teams/MultiTech/code/libmDot-mbed5
+repo rev #a9d4d7cdeca1
+v3.2.1
+
+mbed-os
+========
+https://github.com/ARMmbed/mbed-os/
+repo rev c966348d3f
+v5.11.1
---
+++ src/lora_button/inc/RadioEvent.h
@@ -0,0 +1,42 @@
+#ifndef __RADIO_EVENT_H__
+#define __RADIO_EVENT_H__
+
+#include "dot_util.h"
+#include "mDotEvent.h"
+#include "Fota.h"
+
+class RadioEvent : public mDotEvent
+{
+
+public:
+    RadioEvent();
+
+    virtual ~RadioEvent() {}
+    
+    void setReceiveHandler(void (*onRx)(uint8_t *RxBuffer, uint8_t RxBufferSize));
+
+    virtual void PacketRx(uint8_t port, 
+                          uint8_t *payload, 
+                          uint16_t size, 
+                          int16_t rssi, 
+                          int8_t snr, 
+                          lora::DownlinkControl ctrl, 
+                          uint8_t slot, 
+                          uint8_t retries, 
+                          uint32_t address, 
+                          bool dupRx);
+
+    /*!
+     * MAC layer event callback prototype.
+     *
+     * \param [IN] flags Bit field indicating the MAC events occurred
+     * \param [IN] info  Details about MAC events occurred
+     */
+    virtual void MacEvent(LoRaMacEventFlags* flags, LoRaMacEventInfo* info);
+    
+private:
+    void (*mOnRx)(uint8_t *RxBuffer, uint8_t RxBufferSize);
+};
+
+#endif
+
